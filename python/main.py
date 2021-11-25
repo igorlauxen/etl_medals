8a21e62559a53759f48c8c23af1af9465acf79f5
Igor Lauxen <igor.lauxen@sap.com>
2021-11-24 22:34:33 -0300

    import games

--- python/main.py
+++ python/main.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from country_builder import CountryBuilder
+from game_builder import GameBuilder
 
 input_file_path = '/Users/i841640/Documents/unisinos/big_data_etl/etl_medals/olympics/'
 output_file_path = '/Users/i841640/Documents/unisinos/big_data_etl/etl_medals/olympics/output/'
 
 countryBuilder = CountryBuilder()
 countryBuilder.build(input_file_path, output_file_path)
+
+gameBuilder = GameBuilder()
+gameBuilder.build(input_file_path, output_file_path)
