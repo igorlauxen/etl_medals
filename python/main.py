3d69f422225193d9542d3e4d4d421a0989a003ef
Igor Lauxen <igor.lauxen@sap.com>
2021-11-25 00:08:26 -0300

    use relative path

--- python/main.py
+++ python/main.py
@@ -6,26 +6,31 @@ from team_builder import TeamBuilder
 from coach_builder import CoachBuilder
 from athletes_builder import AthletesBuilder
 
+import os.path
+
+my_path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(my_path, "../olympics/")
+
 # todo improve the load of files
-input_file_path = '/Users/i841640/Documents/unisinos/big_data_etl/etl_medals/olympics/'
-output_file_path = '/Users/i841640/Documents/unisinos/big_data_etl/etl_medals/olympics/output/'
+input_file_path = path
+output_file_path = path+'/output/'
 
 # Note! The order of execution matters!
 
 countryBuilder = CountryBuilder()
-#countryBuilder.build(input_file_path, output_file_path)
+countryBuilder.build(input_file_path, output_file_path)
 
 gameBuilder = GameBuilder()
-#gameBuilder.build(input_file_path, output_file_path)
+gameBuilder.build(input_file_path, output_file_path)
 
 eventBuilder = EventBuilder()
-#eventBuilder.build(input_file_path, output_file_path)
+eventBuilder.build(input_file_path, output_file_path)
 
 medalBuilder = MedalBuilder()
-#medalBuilder.build(input_file_path, output_file_path)
+medalBuilder.build(input_file_path, output_file_path)
 
 teamBuilder = TeamBuilder()
-#teamBuilder.build(input_file_path, output_file_path)
+teamBuilder.build(input_file_path, output_file_path)
 
 coachTeam = CoachBuilder()
 coachTeam.build(input_file_path, output_file_path)
